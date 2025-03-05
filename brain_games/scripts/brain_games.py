0bc089efe4b7513c8469d6409ff995f148c772b6
Maxim <maxtor2008@mail.ru>
2025-03-05 13:49:38 +0300

    part3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,19 @@
+import prompt
+
+
+#text=f'''Welcome to the Brain Games!
+#Hello, {name}'''
+
+
+
 def greet():
     print('Welcome to the Brain Games!')
+    name = prompt.string('May I Have your name?')
+    print(f'''Hello, {name}''')
 
 def main():
     greet()
 
 if __name__ == "__main__":
     main()
+
