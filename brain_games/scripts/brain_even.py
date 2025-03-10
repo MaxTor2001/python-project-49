95bb83df2da9effa8cfcc1e38db4537fc29f3095
Maxim <maxtor2008@mail.ru>
2025-03-10 11:16:42 +0300

    part5

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,24 +1,31 @@
 import random
-count = 0
-while count < 3:
-    array_of_numbers = random.randint(0,1000)
-    print('Answer "yes" if the number is even, otherwise answer  "no"')
-    print('Question:', array_of_numbers)
-    while True:
-        answer = str(input('Your answer: ')).lower()
-        if answer == 'yes' or answer == 'no':
-            break
-        else:
-            print('Incorrect input. Please enter "yes" or "no".')
-    if answer == 'yes' and array_of_numbers % 2 == 0 or answer == 'no' and array_of_numbers % 2 !=0:
-        print('Correct!')
-        count += 1
-    if answer == 'yes' and array_of_numbers % 2 != 0:
-        print("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {name}")
-        count = 0
-        continue
-    if answer == 'no' and array_of_numbers % 2 == 0:
-        print("'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, {name}")
-        count = 0
-        continue
-print('Congratulations, {}!')
+
+def welcome_user():
+    name = input("May I have your name? ")
+    print(f"Hello, {name}!")
+
+    count = 0
+    while count < 3:
+        array_of_numbers = random.randint(0,1000)
+        print('Answer "yes" if the number is even, otherwise answer  "no"')
+        print('Question:', array_of_numbers)
+        while True:
+            answer = str(input('Your answer: ')).lower()
+            if answer == 'yes' or answer == 'no':
+                break
+            else:
+                print('Incorrect input. Please enter "yes" or "no".')
+        if answer == 'yes' and array_of_numbers % 2 == 0 or answer == 'no' and array_of_numbers % 2 !=0:
+            print('Correct!')
+            count += 1
+        if answer == 'yes' and array_of_numbers % 2 != 0:
+            print("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {name}")
+            count = 0
+            continue
+        if answer == 'no' and array_of_numbers % 2 == 0:
+            print("'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, {name}")
+            count = 0
+            continue
+    print('Congratulations, {name}!')
+
+welcome_user()
