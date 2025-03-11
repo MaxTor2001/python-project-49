73fc3677148c76c71a3de6ed597a45d36a4f0cd1
Maxim <maxtor2008@mail.ru>
2025-03-11 20:53:09 +0300

    mistake

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -9,12 +9,13 @@ def welcome_user():
         array_of_numbers = random.randint(0,1000)
         print('Answer "yes" if the number is even, otherwise answer "no"')
         print('Question:', array_of_numbers)
-        while True:
-            answer = str(input('Your answer: ')).lower()
-            if answer == 'yes' or answer == 'no':
-                break
-            else:
-                print('Incorrect input. Please enter "yes" or "no".')
+        answer = str(input('Your answer: ')).lower()
+#        while True:
+#            answer = str(input('Your answer: ')).lower()
+#            if answer == 'yes' or answer == 'no':
+#                break
+#            else:
+#                print('Incorrect input. Please enter "yes" or "no".')
         if answer == 'yes' and array_of_numbers % 2 == 0 or answer == 'no' and array_of_numbers % 2 !=0:
             print('Correct!')
             count += 1
