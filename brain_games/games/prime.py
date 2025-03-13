fda5c238a977314d6f9e8f342a1457cc8b10fe50
Maxim <maxtor2008@mail.ru>
2025-03-13 15:47:50 +0300

    fix

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,54 +1,38 @@
 import random
 
+def is_prime(n):
+    """Check if number is prime."""
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
 def welcome_user():
-    name = input("Welcome to the Brain Games!\nMay I have your name? ")
+    name = input("Welcome to Brain Games!\nMay I have your name? ")
     print(f"Hello, {name}!")
     count = 0
     while count < 3:
         number = random.randint(2, 3571)
-
-        def is_prime(n):
-            if n <= 1:
-                return False
-            for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                    return False
-            return True
-        
-        while True:
-            prompt = (
-                f'Answer "yes" if given number is prime. Otherwise answer "no".\n'
-                f'Question: '
-                f'{number}? '
-            )
-            user_answer = str(input(prompt))
-            if user_answer == 'yes' or user_answer == 'no':
-                break
-            
-            else:
-                print('Incorrect input. Please enter "yes" or "no".')
-
-        if is_prime(number) and user_answer == 'yes':
+        user_answer = input(
+            f"Is {number} prime? (yes/no): "
+        ).lower()
+        while user_answer not in ['yes', 'no']:
+            print("Invalid input. Enter yes or no.")
+            user_answer = input(
+                f"Is {number} prime? (yes/no): "
+            ).lower()
+        if (is_prime(number) and user_answer == 'yes') or \
+           (not is_prime(number) and user_answer == 'no'):
             print("Correct!")
             count += 1
-
-        elif is_prime(number) and user_answer == 'no':
-            print("'no' is wrong answer ;(. Correct answer was 'yes'.)")
-            print(f"Let's try again, {name}!")
-            break
-
-        elif not is_prime(number) and user_answer == 'yes':
-            print("'yes' is wrong answer ;(. Correct answer was 'no'.)")
-            print(f"Let's try again, {name}!")
-            break
-
         else:
-            print("Correct!")
-            count += 1
-
+            correct_answer = 'yes' if is_prime(number) else 'no'
+            print(f"Wrong answer. Correct answer was {correct_answer}.")
+            print(f"Try again, {name}!")
+            break
     if count == 3:
         print(f"Congratulations, {name}!")
-
 
 welcome_user()
