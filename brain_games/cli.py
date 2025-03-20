9939b7c3f24709e14a02a032da50e6ac932f6935
Maxim <maxtor2008@mail.ru>
2025-03-20 11:56:15 +0300

    fix

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,7 +4,3 @@ import prompt
 def welcome_user():
     name = prompt.string("Welcome to the Brain Games!\nMay I have your name? ")
     print(f"Hello, {name}!")
-
-
-if __name__ == "__main__":
-    main()
