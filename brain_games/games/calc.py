0b38654f053466d0d2d220174b605a5beaa38626
MaxTor2001 <maxtor2008@mail.ru>
2025-03-20 11:27:00 +0300

    Add files via upload

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,15 +1,14 @@
 from random import randint, choice
 
 DESCRIPTION = 'What is the result of the expression?'
-START = 1
-STOP = 10
+
 
 
 def generate_question_and_answer():
 
     operation = choice(['+', '-', '*'])
-    num_1 = randint(START, STOP)
-    num_2 = randint(START, STOP)
+    num_1 = randint(1, 10)
+    num_2 = randint(1, 10)
     question = f'{num_1} {operation} {num_2}'
     if operation == '+':
         correct_answer = num_1 + num_2
--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -5,22 +5,17 @@ DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 START = 1
 STOP = 100
 
+ def is_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
 
 def generate_question_and_answer():
-
-    def is_even(number):
-        if number % 2 == 0:
-            return True
-        else:
-            return False
-
     number = randint(START, STOP)
     question = f'{number}'
 
-    if is_even(number):
-        correct_answer = 'yes'
-    else:
-        correct_answer = 'no'
+    correct_answer = 'yes' if is_even(number) else 'no'
 
     return question, correct_answer
 
